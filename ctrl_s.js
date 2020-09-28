fa4bb79aee1f4af0bcec4fe84fbb81fec9a409e6
Levent Yalcin <leventyalcin@gmail.com>
2020-09-28 13:43:25 +0100

    AWS Search changes for the new UI and README updates

--- ctrl_s.js
+++ ctrl_s.js
@@ -1,3 +1,3 @@
 if ( window.location.hostname.match("console.aws.amazon.com$") == "console.aws.amazon.com" ) {
-  document.getElementById('nav-servicesMenu').click();
+  document.querySelectorAll('[data-testid="aws-services-list-button"]')[0].click()
 }
