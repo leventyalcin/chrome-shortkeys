ffb66795dee5b9e160a03e2b07b75a67478e7738
Levent Yalcin <leventyalcin@gmail.com>
2020-09-28 13:44:02 +0100

    Region shortcuts for eu-west-*

---
+++ ctrl_shift_2.js
@@ -0,0 +1,3 @@
+if ( window.location.hostname.match("console.aws.amazon.com$")) {
+    window.location.href = window.location.protocol + "//eu-west-2.console.aws.amazon.com" + window.location.pathname;
+}
---
+++ ctrl_shift_3.js
@@ -0,0 +1,3 @@
+if ( window.location.hostname.match("console.aws.amazon.com$")) {
+    window.location.href = window.location.protocol + "//eu-west-3.console.aws.amazon.com" + window.location.pathname;
+}
